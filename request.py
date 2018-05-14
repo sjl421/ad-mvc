2a10c5dd71599463abc492ecc9b870125d9cae71
accelad <accelad@outlook.com>
2018-05-14 22:32:07 +0800

    完成 Request 类编写，实现 request 解析

---
+++ request.py
@@ -0,0 +1,79 @@
+import json
+from urllib.parse import unquote_plus
+
+
+class Reqeust(object):
+    def __init__(self, raw_data):
+        self.raw_data = raw_data
+        self.method = ''
+        self.path = ''
+        self.headers = {}
+        self.cookies = {}
+        self.args = {}
+        self.body = ''
+
+        self.setup()
+
+    def __repr__(self):
+        properties = ['{}: ({})'.format(k, v) for k, v in self.__dict__.items()]
+        s = '\n'.join(properties)
+        return '< {} >\n'.format(s)
+
+    def setup(self):
+        header, body = self.raw_data.split('\r\n\r\n', 1)
+        self.parse_header(header)
+        self.body = body
+
+    def parse_header(self, header):
+        h, content = header.split('\r\n', 1)
+        h = h.split()
+
+        self.method = h[0]
+        self.parse_path(h[1])
+
+        content = content.split('\r\n')
+        headers = {}
+        for c in content:
+            k, v = c.split(': ')
+            headers[k] = v
+        self.headers = headers
+
+        if 'Cookie' in headers:
+            cookie = headers['Cookie'].split('; ')
+            cookies = {}
+            for c in cookie:
+                k, v = c.split('=')
+                cookies[k] = v
+            self.cookies = cookies
+
+    def parse_path(self, path):
+        path = unquote_plus(path)
+
+        if '?' in path:
+            path, params_string = path.split('?', 1)
+            self.path = path
+
+            params = params_string.split('&')
+            args = {}
+            for p in params:
+                k, v = p.split('=')
+                args[k] = v
+            self.args = args
+        else:
+            self.path = path
+
+    def form(self):
+        body = unquote_plus(self.body)
+
+        form = {}
+        if '&' in body:
+            params = body.split('&')
+            for p in params:
+                k, v = p.split('=')
+                form[k] = v
+
+        return form
+
+    def json(self):
+        body = unquote_plus(self.body)
+        return json.loads(body)
--- server.py
+++ server.py
@@ -2,20 +2,26 @@ import _thread
 import socket
 
 from utils import log
+from request import Reqeust
 
 
 def process_reqeust(connection):
     with connection as con:
-        request = b''
+        raw_data = b''
         buffer_size = 1024
         while True:
             r = con.recv(buffer_size)
-            request += r
+            raw_data += r
             if len(r) < buffer_size:
                 break
-        log('接收到 request <{}>'.format(request.decode()))
 
-        con.sendall(b'recived!')
+        raw_data = raw_data.decode()
+        log('接收到 raw_data <{}>'.format(raw_data))
+        if len(raw_data) > 0:
+            request = Reqeust(raw_data)
+            log('接收到 request {}'.format(request))
+
+            con.sendall(b'recived!')
 
 
 def run(host, port):
