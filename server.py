d5506641a1b207585a0ce838b2b15179b31ee36e
accelad <accelad@outlook.com>
2018-05-14 20:38:32 +0800

    完成 socket 收发

---
+++ server.py
@@ -0,0 +1,36 @@
+import _thread
+import socket
+
+
+def process_reqeust(connection):
+    with connection as con:
+        request = b''
+        buffer_size = 1024
+        while True:
+            r = con.recv(buffer_size)
+            request += r
+            if len(r) < buffer_size:
+                break
+        print('接收到 request <{}>'.format(request.decode()))
+        con.sendall(b'recived!')
+
+
+def run(host, port):
+    """
+    启动服务器
+    """
+    with socket.socket() as s:
+        s.bind((host, port))
+        s.listen()
+        print('服务器监听启动 http://{}:{}'.format(host, port))
+        while True:
+            connection, address = s.accept()
+            _thread.start_new_thread(process_reqeust, (connection,))
+
+
+if __name__ == '__main__':
+    config = dict(
+        host="127.0.0.1",
+        port=3000,
+    )
+    run(**config)
