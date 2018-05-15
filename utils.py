cc08a5a846ca46a693484943ccbe9edb227203dd
accelad <accelad@outlook.com>
2018-05-15 20:13:22 +0800

    修复 formatted 拼写错误

--- utils.py
+++ utils.py
@@ -1,10 +1,11 @@
 import time
 
+
 def log(*args, **kwargs):
     time_format = '---%Y/%m/%d %H:%M:%S---'
     local_time = time.localtime(time.time())
-    formated_time = time.strftime(time_format, local_time)
+    formatted_time = time.strftime(time_format, local_time)
 
     with open('ad-web.log', 'a+', encoding='utf-8') as f:
-        print(formated_time, *args, **kwargs)
-        print(formated_time, *args, file=f, **kwargs)
+        print(formatted_time, *args, **kwargs)
+        print(formatted_time, *args, file=f, **kwargs)
