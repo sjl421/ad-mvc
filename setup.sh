615f043dd47aedcae1dbc6ec5fb42567b133eec3
accelad <accelad@outlook.com>
2018-05-18 23:36:14 +0800

    添加 redis 相关的部署脚本

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 apt update
-apt install -y python3 python3-pip supervisor
-pip3 install jinja2
+apt install -y python3 python3-pip redis supervisor
+pip3 install jinja2 redis
 
 ln -s -f /var/www/ad-mvc/supervisor.conf /etc/supervisor/conf.d/ad-mvc.conf
 
+service redis-server restart
 service supervisor restart
