34aca176ee4dc3f1ed95e0a7c44040c48e8ebe29
accelad <accelad@outlook.com>
2018-05-18 23:04:05 +0800

    使用 jinja2 的模板继承，简化前端代码

--- static/utils.js
+++ static/utils.js
@@ -29,27 +29,23 @@ var escapedHTML = function(html) {
 // ajax 函数
 var ajax = function(method, path, data, responseCallback) {
     var r = new XMLHttpRequest()
-    // 设置请求方法和请求地址
     r.open(method, path, true)
-    // 设置发送的数据的格式为 application/json
     r.setRequestHeader('Content-Type', 'application/json')
-    // 注册响应函数
+
     r.onreadystatechange = function() {
         // 4 表示 response 接收完毕
         if(r.readyState === 4) {
-            // r.response 存的就是服务器发过来的放在 HTTP BODY 中的数据
             var response = JSON.parse(r.response)
             // ajax 无法直接完成跳转，此处约定“用一个 error 字段作为失败的标记”
-            var error = response.error
-            if (error) {
-                alert(error)
+            if (response.error) {
+                alert(response.error)
             } else {
+                // 注册好的回调
                 responseCallback(response)
             }
         }
     }
-    // 把数据转换为 json 格式字符串
+
     var data = JSON.stringify(data)
-    // 发送请求
     r.send(data)
 }
