b43da839a2015cd696690fa911183d7763751d84
accelad <accelad@outlook.com>
2018-05-20 05:41:31 +0800

    部署环境切换到 ubuntu

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
+add-apt-repository -y ppa:deadsnakes/ppa
 apt update
-apt install -y python3 python3-pip redis supervisor
+
+apt install -y python3.6 redis-server supervisor
+
+curl https://bootstrap.pypa.io/get-pip.py > /tmp/get-pip.py
+python3.6 /tmp/get-pip.py
+
 pip3 install jinja2 redis
 
 ln -s -f /var/www/ad-mvc/supervisor.conf /etc/supervisor/conf.d/ad-mvc.conf
